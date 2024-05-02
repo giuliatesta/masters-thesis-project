3552c0b5ae9b7a92cc62157032ab3b59ec491fea
Giulia Testa <giuliatestax@gmail.com>
2024-05-02 17:29:46 +0200

    fix bug filtering the first n rows from the dataset and updated the plots

--- network_building.py
+++ network_building.py
@@ -8,7 +8,7 @@ from networkx import Graph, draw
 from pandas import DataFrame
 from gower import gower_matrix
 
-from statistics import gower_matrix_distribution
+from preprocessing import remove_all_except_for, remove_column
 
 
 def create_network(df: DataFrame, similarity_threshold: float, name="Network", no_logs=False):
@@ -16,18 +16,21 @@ def create_network(df: DataFrame, similarity_threshold: float, name="Network", n
         print("Creating the network...")
     # creates the graph
     graph = Graph(name=name)
+
     # add all the nodes
-    print(f"data: {len(df)}")
+    # sort the dataframe by index to ensure consistent iteration order
+    nodes_counter = 0
     for index, row in df.iterrows():
         graph.add_node(index, attr_dict=row.to_dict())
+        nodes_counter = nodes_counter +1
     if not no_logs:
-        print(f"Added {len(df)} nodes")
+        print(f"Added {nodes_counter} nodes")
     # computes the Gower's distance for creating the edges in the network
     # a smaller distance value indicates higher similarities between data points
     similarities = 1.0 - gower_matrix(df)
     # gower_matrix_distribution(similarities)
     counter = 0
-    for edge in itertools.combinations(range(len(df)), 2):  # creates all combinations of possible edges
+    for edge in sorted(itertools.combinations(range(len(df)), 2)):  # creates all combinations of possible edges
         i = edge[0]
         j = edge[1]
         similarity = similarities[i, j]
@@ -48,6 +51,7 @@ def plot_network(graph: Graph, file_path=""):
     if file_path == "":
         file_path = get_file_path(graph_name=graph.name)
     print(f"Saving in {file_path}")
+    plt.legend([f'Nodes: {len(graph.nodes)}', f'Edges: {len(graph.edges)}'], loc='upper right')
     plt.savefig(file_path)
     plt.close()
     print("Done.")
@@ -60,3 +64,25 @@ def get_file_path(graph_name):
         os.makedirs(dir_name)
 
     return f"{dir_name}/{graph_name}.png"
+
+
+# for creating the networks with only one column at the time.
+def create_networks_using_one_column(df: DataFrame, threshold: float):
+    data = df
+    for column in data.columns:
+        removed_data = remove_all_except_for(data, [column])
+        graph = create_network(removed_data, similarity_threshold=threshold,
+                               name=f"Network with only {column}", no_logs=True)
+        plot_network(graph)
+        data = df
+
+
+# for creating the networks removing one column at the time
+def create_networks_removing_one_column(df: DataFrame, threshold: float):
+    data = df
+    for column in data.columns:
+        removed_data = remove_column(data, column_name=column)
+        graph = create_network(removed_data, similarity_threshold=threshold,
+                               name=f"Network with only {column}")
+        plot_network(graph)
+        data = df
