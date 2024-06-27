7c403e9796771313076f1e2be6a864c529506abb
Giulia Testa <giuliatestax@gmail.com>
2024-06-27 16:47:44 +0200

    removes the index when loading the csv file

--- preprocessing.py
+++ preprocessing.py
@@ -7,10 +7,12 @@ from pandas import DataFrame
 # index_col = 0 means that it uses the first column as index for the rows
 # header = 0 means that the first row contains the columns' names
 # encoding= cp1252 since some values cannot be read by default utf8
-def load_dataset_csv(path: str):
+def load_dataset_csv(path: str, index: bool):
     print(f"Loading the dataset from {path}")
     dataset = pd.read_csv(path, sep=',', header=0, encoding="cp1252", dtype=str)
     dataset.replace("", np.nan, inplace=True)  # Replace empty strings with NaN
+    if not index:
+        dataset.reset_index(drop=True, inplace=True)
     dataset.dropna(inplace=True)
     print(f"Loaded {len(dataset)} records")
     return dataset
@@ -23,7 +25,7 @@ def filter_by(df: DataFrame, column_name: str, column_value: str, number_of_rows
 
     # if I want to get the first n values
     if number_of_rows != -1:
-        filtered = filtered.iloc[:number_of_rows]
+        filtered = filtered.head[number_of_rows]
     print(f"Filtered {len(filtered)} data out of {len(df)} rows")
     filtered.reset_index(drop=True, inplace=True)
     return filtered
