rerun the sim_01 with 30 runs + trying to compute the states from the final vls average (but something it's still wrong)

agent-based-basic/UTILS.py
@@ -3,7 +3,7 @@ import os
 
 import numpy as np
 
-from conf import LABELS, STATE_CHANGING_METHOD, RESULTS_DIR
+from conf import LABELS, STATE_CHANGING_METHOD
 
 STATE = "_LPStates"
 # / + "log_trial_"
@@ -91,44 +91,3 @@ def read_pickled_file(file_path):
         data = pickle.load(file)
     return data
 
-
-def average_results(results_directory, file_picker, results_file_name):
-    results = []
-    avg_results = []
-    # reads the results file in the directory and stored them in the results array
-    for filename in os.listdir(results_directory):
-        if file_picker(filename):
-            file_name = os.path.join(results_directory, filename)
-            data = read_pickled_file(file_name)
-            results.append(data)
-
-    # accumulates the vectors for each time step
-    accumulated = {}
-    for run in results:
-        for time_step, vectors in run:
-            if time_step not in accumulated:
-                accumulated[time_step] = []
-            if len(accumulated[time_step]) == 0:
-                accumulated[time_step] = np.array(vectors)
-            else:
-                accumulated[time_step] = np.add(accumulated[time_step], vectors)
-
-    # averages the accumulated vectors and adds the result to the final avg results array
-    for time_step in sorted(accumulated.keys()):
-        average_vectors = accumulated[time_step] / len(results)
-        avg_results.append([time_step, average_vectors.tolist()])
-
-    # stores the averaged results in a pickled file called avg_results
-    avg_file_path = results_directory + "/" + results_file_name
-    store_to_file(list(avg_results), avg_file_path)
-    print(f"---- AVERAGED RESULTS: {results_file_name}----")
-    print_pickled_file(avg_file_path)
-    return avg_results
-
-
-def average_vector_labels(filename):
-    return filename.startswith("trial_") and not filename.endswith("STATES.pickled")
-
-
-def average_states(filename):
-    return filename.endswith("STATES.pickled")