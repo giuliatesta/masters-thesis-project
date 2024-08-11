trying to correct the averaging method for the states (returns only non adapters - both with averaging and majority rule)

agent-based-basic/average_results.py
@@ -39,6 +39,7 @@ def average_vls_results(results_directory, results_file_name, ):
     # UTILS.print_pickled_file(avg_file_path)
     return avg_results
 
+
 def average_state_results(results_directory, results_file_name, ):
     results = []
     avg_results = []
@@ -63,9 +64,13 @@ def average_state_results(results_directory, results_file_name, ):
     # checks the sign of the accumalated value: if positive, then ADAPTER (+1); if negative, NON ADAPTER (-1)
     for time_step in sorted(accumulated.keys()):
         summed_vectors = accumulated[time_step]
-        binary_average_vectors = np.sign(summed_vectors)
-        print(f"{summed_vectors} -> {binary_average_vectors}")
-        avg_results.append([time_step, binary_average_vectors.tolist()])
+        # signs = np.sign(summed_vectors)
+        avgs = summed_vectors / len(results)
+
+        # Step 5: Determine final states: +1 if the average is >= 0, else -1
+        signs = np.where(avgs >= 0, 1, -1)
+        print(f"{summed_vectors} ->\n {avgs},\n{signs}")
+        avg_results.append([time_step, signs.tolist()])
 
     # stores the averaged results in a pickled file called avg_results
     avg_file_path = results_directory + "/" + results_file_name
@@ -73,41 +78,3 @@ def average_state_results(results_directory, results_file_name, ):
     print(f"---- AVERAGED STATES: {results_file_name}----")
     UTILS.print_pickled_file(avg_file_path)
     return avg_results
-
-# def get_final_state_from_vls(averaged_vls, results_file_name):
-#     def determine_state(vl):
-#         first = vl[0]
-#         second = vl[1]
-#         if first >= second:
-#             # if the non adapter is greater than adapter -> it becomes non adapter
-#             return 1
-#         elif first < second:
-#             print(f"first:{first}, second: {second}")
-#             # if the adapter is greater than non adapter -> it becomes adapter
-#             return -1
-#
-#     states = [[i[0], [ for _ in range(len(i[1]))]] for i in averaged_vls]
-#     print(f"states: {states}")
-#     for i, run in enumerate(averaged_vls):
-#         time_step = run[0]
-#         vectors = run[1]
-#         for vector in vectors:
-#             # states[time_step] = []
-#             states[i][1].append(determine_state(vector))      # TODO append state given the vectors
-#         print(f"avg_results[time_step]: {states[i]}")
-#
-#     #
-#     # # averages the accumulated vectors and adds the result to the final avg results array
-#     # # print(f"accumulated: {accumulated}")
-#     # for time_step in sorted(accumulated.keys()):
-#     #     average_vectors = accumulated[time_step] / len(results)
-#     #     # print(f"average vector {time_step}: {average_vectors}")
-#     #     avg_results.append([time_step, average_vectors.tolist()])
-#     #
-#     # # stores the averaged results in a pickled file called avg_results
-#     # avg_file_path = results_directory + "/" + results_file_name
-#     UTILS.store_to_file(list(states), results_file_name)
-#     print(f"---- AVERAGED RESULTS: {results_file_name}----")
-#     print(states)
-#     # # print_pickled_file(avg_file_path)
-#     # return avg_results