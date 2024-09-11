attempt to correct the averaging process (still wrong the vls for the run 0)

agent-based-basic/average_results.py
@@ -4,7 +4,7 @@ import numpy as np
 import UTILS
 
 
-def average_vls_results(results_directory, results_file_name, ):
+def average_vls_results(results_directory, results_file_name):
     results = []
     avg_results = []
     # reads the results file in the directory and stored them in the results array
@@ -16,8 +16,10 @@ def average_vls_results(results_directory, results_file_name, ):
 
     # accumulates the vectors for each time step
     accumulated = {}
-    for run in results:
-        for time_step, vectors in run:
+    for runs in results:
+        for run in runs:
+            time_step = run[0]
+            vectors = run[1]
             if time_step not in accumulated:
                 accumulated[time_step] = []
             if len(accumulated[time_step]) == 0:
@@ -29,52 +31,24 @@ def average_vls_results(results_directory, results_file_name, ):
     # print(f"accumulated: {accumulated}")
     for time_step in sorted(accumulated.keys()):
         average_vectors = accumulated[time_step] / len(results)
-        # print(f"average vector {time_step}: {average_vectors}")
+        # print(f"average vector {time_step}: {accumulated[time_step]}/ { len(results)} = {average_vectors}")
         avg_results.append([time_step, average_vectors.tolist()])
 
     # stores the averaged results in a pickled file called avg_results
     avg_file_path = results_directory + "/" + results_file_name
-    UTILS.store_to_file(list(avg_results), avg_file_path)
+    # UTILS.store_to_file(list(avg_results), avg_file_path)
     # print(f"---- AVERAGED RESULTS: {results_file_name}----")
-    # UTILS.print_pickled_file(avg_file_path)
+    # UTILS.print_pickled_file(avg_file_path, 2)
     return avg_results
 
 
-def average_state_results(results_directory, results_file_name, ):
-    results = []
-    avg_results = []
-    # reads the results file in the directory and stored them in the results array
-    for filename in sorted(os.listdir(results_directory)):
-        if filename.endswith("STATES.pickled"):
-            file_name = os.path.join(results_directory, filename)
-            data = UTILS.read_pickled_file(file_name)
-            results.append(data)
+def calculate_states_from_averaged_vls(vls, results_directory, results_file_name):
+    def determine_state(vl):
+        return -1 if vl[0] > vl[1] else 1
 
-    # accumulates the vectors for each time step
-    accumulated = {}
-    for run in results:
-        for time_step, vectors in run:
-            if time_step not in accumulated:
-                accumulated[time_step] = []
-            if len(accumulated[time_step]) == 0:
-                accumulated[time_step] = np.array(vectors)
-            else:
-                accumulated[time_step] = np.add(accumulated[time_step], vectors)
-
-    # checks the sign of the accumalated value: if positive, then ADAPTER (+1); if negative, NON ADAPTER (-1)
-    for time_step in sorted(accumulated.keys()):
-        summed_vectors = accumulated[time_step]
-        # signs = np.sign(summed_vectors)
-        avgs = summed_vectors / len(results)
-
-        # determine final states: +1 if the average is >= 0, else -1
-        signs = np.where(avgs >= 0, 1, -1)
-        # print(f"{summed_vectors} ->\n {avgs},\n{signs}")
-        avg_results.append([time_step, signs.tolist()])
-
-    # stores the averaged results in a pickled file called avg_results
+    states = [[run[0], [determine_state(vectors) for vectors in run[1]]] for run in vls]
     avg_file_path = results_directory + "/" + results_file_name
-    UTILS.store_to_file(list(avg_results), avg_file_path)
+    UTILS.store_to_file(list(states), avg_file_path)
     print(f"---- AVERAGED STATES: {results_file_name}----")
     UTILS.print_pickled_file(avg_file_path)
-    return avg_results
+    return states