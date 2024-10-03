a00273e6223d129f2ea6d1f24719aefe52e2c4fd
Giulia Testa <giuliatestax@gmail.com>
2024-10-03 18:50:27 +0200

    added slope to the adapters by time step plot and custom title

--- agent-based-basic/simulation_result_plotter.py
+++ agent-based-basic/simulation_result_plotter.py
@@ -9,22 +9,27 @@ from utils import read_pickled_file
 BASE_LINE = {1: 100, 2: 513, 3: 719, 4: 828, 5: 887, 6: 924, 7: 948, 8: 963, 9: 973, 10: 980, 11: 984, 13: 991, 14: 993, 15: 994, 16: 995, 17: 996, 18: 997, 19: 997, 20: 998, 21: 998, 22: 998, 23: 998, 24: 998, 25: 999, 26: 999, 27: 999, 28: 999, 29: 999, 30: 999, 31: 999}
 
 
-def draw_adapter_by_time_plot(adapters, results_file_path):
+def draw_adapter_by_time_plot(adapters, results_file_path, title):
     plt.figure(figsize=(10, 6))
     plt.plot(list(adapters.keys()), list(adapters.values()), marker='x', label="Current simulation")
-    convergence_value = list(adapters.values())[-1]
+    last_value = list(adapters.values())[-1]
+    first_value = list(adapters.values())[0]
     for key, value in adapters.items():
-        if value == convergence_value:
+        if value == last_value:
             # plt.plot(key, value, marker='o')
-            plt.axvline(x=key, linestyle='--', color=('cyan', 0.3), label='Convergence threshold')
+            plt.axvline(x=key, linestyle='--', color=('cyan', 0.5), label='Convergence threshold')
             break
+    slope = (last_value-first_value) / len(adapters)
+    # 0, 990
+    plt.text(28, 240, f"slope: {slope:.2f}", fontsize=10,
+             bbox=dict(facecolor='none', alpha=0.2))
     # for key, value in adapters.items():
     #     print(f"({key}, {value}")
     #     plt.text(key, value, f"({key:.2f}, {value})", fontsize=6, ha="center")
     plt.plot(list(BASE_LINE.keys()), list(BASE_LINE.values()), 'g--', linewidth=1, label='Base Line')
     plt.xlabel('Time steps')
     plt.ylabel('Adapters')
-    plt.title('Number of adapters by time')
+    plt.title('Number of adapters by time' if title == "" else title)
     plt.legend(loc='lower right')
     plt.grid(True)
     plt.savefig(f"{results_file_path}/avg_adapters_by_time_plot.png", dpi=1000)
