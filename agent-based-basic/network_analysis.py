dd974cd91cb06ee5b03df6684b504181d76e86fb
Giulia Testa <giuliatestax@gmail.com>
2024-10-07 15:06:10 +0200

    degree distribution of the network

--- agent-based-basic/network_analysis.py
+++ agent-based-basic/network_analysis.py
@@ -2,6 +2,14 @@ import networkx as nx
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+#Network analysis for LPA Network
+# Number ofn nodes: 1000
+# Number of edges: 243727
+# Density of the network: 0.48794194194194196
+# Average clustering: 0.615123305197661
+# Diameter: 3
+# Average shortest path: 1.5120940940940941
+
 class NetworkAnalysis:
 
     def __init__(self, graph):
@@ -13,9 +21,23 @@ class NetworkAnalysis:
         print(f"Number of edges: {self.graph.number_of_edges()}")
         print(f"Density of the network: {nx.density(self.graph)}")
         print("------------------------------------------------")
-        print(f"Degree centrality: {nx.degree_centrality(self.graph)}")
-        print(f"Degree centrality")
-
+        print(f"Diameter: {nx.diameter(self.graph)}")
+        print(f"Average shortest path: {nx.average_shortest_path_length(self.graph)}")
+        print("------------------------------------------------")
+        print("Degree distribution plot")
+        degrees = [degree for node, degree in self.graph.degree()]
+        plt.figure()
+        plt.hist(degrees, bins=range(min(degrees), max(degrees) + 2), density=True, alpha=0.5, color='mediumorchid',
+                 edgecolor='mediumorchid')
+        plt.title("Degree Distribution")
+        plt.xlabel("Degree")
+        plt.ylabel("Density")
+        plt.grid(True)
+        path = "/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/degree_distribution.png"
+        plt.savefig(path, dpi=1200)
+        print(f"saved in {path}")
+        print("------------------------------------------------")
+        # print(f"Degree centrality: {nx.degree_centrality(self.graph)}")
         plt.figure()
         plt.subplot(2, 2, 1)
         sns.kdeplot(nx.degree_centrality(self.graph), fill=True, legend=True, color="mediumorchid")
@@ -24,6 +46,7 @@ class NetworkAnalysis:
         path = "/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/network_degree_centrality.png"
         #plt.savefig(path, dpi=1200)
         print(f"saved in {path}")
+
         # print(f"Betweenness centrality: {nx.betweenness_centrality(self.graph)}")
         plt.subplot(2, 2, 2)
         sns.kdeplot(nx.betweenness_centrality(self.graph), fill=True, legend=True, color="mediumorchid")
@@ -32,6 +55,7 @@ class NetworkAnalysis:
         path = "/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/network_betweenness_centrality.png"
         # plt.savefig(path, dpi=1200)
         print(f"saved in {path}")
+
         # print(f"Closeness centrality: {nx.closeness_centrality(self.graph)}")
         plt.subplot(2, 2, 3)
         sns.kdeplot(nx.closeness_centrality(self.graph), fill=True, legend=True, color="mediumorchid")
@@ -40,6 +64,7 @@ class NetworkAnalysis:
         path = "/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/network_closeness_centrality.png"
         #plt.savefig(path, dpi=1200)
         print(f"saved in {path}")
+
         # print(f"Eigenvector centrality: {nx.eigenvector_centrality(self.graph)}")
         plt.subplot(2,2, 4)
         sns.kdeplot(nx.eigenvector_centrality(self.graph), fill=True, legend=True, color="mediumorchid")
@@ -49,15 +74,7 @@ class NetworkAnalysis:
         path = "/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/network_eigenvector_centrality.png"
         plt.savefig("/Users/giuliatesta/PycharmProjects/masters-thesis-project/plots/statistics/centralities.png", dpi=1200)
         print(f"saved in {path}")
-        # print("------------------------------------------------")
-        print(f"Average clustering: {nx.average_clustering(self.graph)}")
-        # print(f"Connected components: {nx.connected_components(self.graph)}")
-        print("------------------------------------------------")
-        print(f"Diameter: {nx.diameter(self.graph)}")
-        print(f"Average shortest path: {nx.average_shortest_path_length(self.graph)}")
-        print(f"Shortest path: {nx.shortest_path_length(self.graph)}")
 
-        print(f"Degree distribution")
         plt.figure()
         degree_distribution = [degree for _, degree in self.graph.degree()]
         sns.kdeplot(degree_distribution, fill=True, legend=True, color="mediumorchid")
