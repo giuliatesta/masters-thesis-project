274efdb6e104004c8fd9706d56231048e90889e7
Giulia Testa <giulia.testa@in4matic.it>
2024-11-28 19:59:47 +0100

    FINAL SS (still need to redo plots)

--- agent-based-basic/main_LPA.py
+++ agent-based-basic/main_LPA.py
@@ -95,7 +95,7 @@ initialisation_choices = {
 # biases is introduced by using SI as perc for becoming adopter
 # the different type of biases depends on the moment of application
 all_cognitive_biases = {
-   0: "no-bias",
+ #  0: "no-bias",
    1: "confirmation-bias",  # if the majority of neighbours is non adopters
    2: "availability-bias",  # if the majority of neighbours is adopter
    3: "confirmation-availability-bias"  # in any case
@@ -115,14 +115,14 @@ RUNS = 5  # 30
 SIMILARITY_THRESHOLD = 0.60
 ALPHA = 2
 BETA = 2
-VL_UPDATE_METHOD = vector_labels_update_choices[6]
+VL_UPDATE_METHOD = vector_labels_update_choices[5]
 INITIALISATION = initialisation_choices[0]
 INITIAL_ADAPTERS_PERC = 5
-APPLY_COGNITIVE_BIAS = all_cognitive_biases[0]
+APPLY_COGNITIVE_BIAS = all_cognitive_biases[1]
 APPLY_SOCIAL_BIAS = all_social_biases[4]
 if __name__ == '__main__':
     for bias in all_cognitive_biases.values():
-        APPLY_SOCIAL_BIAS = bias
+        APPLY_COGNITIVE_BIAS = bias
         additional_dir = bias.upper()
         counter = 1
         for init_type in initialisation_choices.values():
