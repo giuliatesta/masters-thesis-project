6243d9bbc856d859bb8f671b6fc60d128e19f403
Giulia Testa <giulia.testa@in4matic.it>
2024-11-30 00:09:52 +0100

    overconfidence

--- agent-based-basic/main_LPA.py
+++ agent-based-basic/main_LPA.py
@@ -95,14 +95,14 @@ initialisation_choices = {
 # biases is introduced by using SI as perc for becoming adopter
 # the different type of biases depends on the moment of application
 all_cognitive_biases = {
-  # 0: "no-bias",
-   1: "confirmation-bias",  # if the majority of neighbours is non adopters
-   2: "availability-bias",  # if the majority of neighbours is adopter
-   3: "confirmation-availability-bias"  # in any case
+   0: "no-bias",
+  # 1: "confirmation-bias",  # if the majority of neighbours is non adopters
+  # 2: "availability-bias",  # if the majority of neighbours is adopter
+  # 3: "confirmation-availability-bias"  # in any case
 }
 
 all_social_biases = {
-     4: "no-bias",
+    # 4: "no-bias",
      5: "against-opposite-gender",
      6: "against-women",
      7: "against-young",
@@ -115,14 +115,14 @@ RUNS = 5  # 30
 SIMILARITY_THRESHOLD = 0.60
 ALPHA = 2
 BETA = 2
-VL_UPDATE_METHOD = vector_labels_update_choices[6]
+VL_UPDATE_METHOD = vector_labels_update_choices[2]
 INITIALISATION = initialisation_choices[0]
 INITIAL_ADAPTERS_PERC = 5
-APPLY_COGNITIVE_BIAS = all_cognitive_biases[1]
-APPLY_SOCIAL_BIAS = all_social_biases[4]
+APPLY_COGNITIVE_BIAS = all_cognitive_biases[0]
+APPLY_SOCIAL_BIAS = all_social_biases[5]
 if __name__ == '__main__':
-    for bias in all_cognitive_biases.values():
-        APPLY_COGNITIVE_BIAS = bias
+    for bias in all_social_biases.values():
+        APPLY_SOCIAL_BIAS = bias
         additional_dir = bias.upper()
         counter = 1
         for init_type in initialisation_choices.values():
